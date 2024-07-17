08f77a6240d401cc1df9eee6a7f0f6b280b69e52
SabinaSayfullayeva <ssayfullayeva50@gmail.com>
2024-07-17 16:41:14 +0300

    commit all

--- src/main/java/uz/resultme/service/PartnerService.java
+++ src/main/java/uz/resultme/service/PartnerService.java
@@ -1,6 +1,5 @@
 package uz.resultme.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import uz.resultme.entity.Partner;
 import uz.resultme.entity.Photo;
-import uz.resultme.exception.IllegalPhotoTypeException;
+import uz.resultme.entity.article.Article;
 import uz.resultme.payload.ApiResponse;
 import uz.resultme.payload.PartnerDTO;
 import uz.resultme.repository.PartnerRepository;
@@ -23,29 +22,48 @@ public class PartnerService
     private final PartnerRepository partnerRepository;
     private final PhotoService photoService;
 
-    public ResponseEntity<ApiResponse<PartnerDTO>> addPartner(MultipartFile file, String url)
+    public ResponseEntity<ApiResponse<Partner>> addPartner( Partner partner)
     {
 
-        ApiResponse<PartnerDTO> response = new ApiResponse<>();
-        ObjectMapper objectMapper = new ObjectMapper();
-        if (file.getContentType() == null ||
-                !(file.getContentType().equals("image/png") ||
-                        file.getContentType().equals("image/svg+xml")))
-        {
-            throw new IllegalPhotoTypeException("Unsupported image type :" + file.getContentType());
-        }
+        ApiResponse<Partner> response = new ApiResponse<>();
 
-        Photo photo = photoService.save(file);
-        Partner partner = new Partner(photo, url);
-        partnerRepository.save(partner);
+
+
+        Partner partner1 = new Partner();
+        partner1.setPartnerUrl(partner.getPartnerUrl());
+        Partner save = partnerRepository.save(partner1);
 
         response.setMessage("Created");
-        response.setData(new PartnerDTO(partner));
+        response.setData(save);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    public ResponseEntity<ApiResponse<Partner>> updatePartner(Long id, MultipartFile file, String url)
+    public ResponseEntity<ApiResponse<Partner>> uploadImage(Long id ,MultipartFile file){
+        ApiResponse<Partner> response = new ApiResponse<>();
+
+        if (!(file.getContentType().equals("image/png") ||
+                file.getContentType().equals("image/svg+xml")))
+        {
+            response.setMessage("Invalid file , only image/png or image/svg+xml");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        if (!partnerRepository.existsById(id)) {
+            response.setMessage("Article with id " + id + " does not exist");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        Partner partner = partnerRepository.findById(id).get();
+        Photo main = photoService.save(file);
+        partner.setPhoto(main);
+
+        Partner save = partnerRepository.save(partner);
+        response.setMessage("Photo succesfully saved");
+        response.setData(save);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<ApiResponse<Partner>> updatePartner(Long id, Partner partner)
     {
         ApiResponse<Partner> response = new ApiResponse<>();
         if (!partnerRepository.existsById(id))
@@ -54,21 +72,21 @@ public class PartnerService
             return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
         Partner newPartner = partnerRepository.findById(id).get();
-        Photo oldPhoto = newPartner.getPhoto();
+       // Photo oldPhoto = newPartner.getPhoto();
 
-        if (file == null || file.isEmpty())
+        /*if (file == null || file.isEmpty())
         {
             newPartner.setPhoto(oldPhoto);
         } else
         {
             newPartner.setPhoto(photoService.save(file));
-        }
-        if (url == null || url.isEmpty())
+        }*/
+        if (partner.getPartnerUrl() == null || partner.getPartnerUrl().isEmpty())
         {
             newPartner.setPartnerUrl(newPartner.getPartnerUrl());
         } else
         {
-            newPartner.setPartnerUrl(url);
+            newPartner.setPartnerUrl(partner.getPartnerUrl());
         }
         newPartner.setId(id);
         partnerRepository.save(newPartner);
@@ -78,6 +96,43 @@ public class PartnerService
         return new ResponseEntity<>(response, HttpStatus.OK);
 
     }
+
+    public ResponseEntity<ApiResponse<Partner>> updateImage(Long id,MultipartFile file){
+
+        ApiResponse<Partner> response = new ApiResponse<>();
+
+        if (!(file.getContentType().equals("image/png") ||
+                file.getContentType().equals("image/svg+xml")))
+        {
+            response.setMessage("Invalid file , only image/png or image/svg+xml");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        if (!partnerRepository.existsById(id))
+        {
+            response.setMessage("Partner with id " + id + " does not exist");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+        Partner newPartner = partnerRepository.findById(id).get();
+         Photo oldPhoto = newPartner.getPhoto();
+
+        if (file == null || file.isEmpty())
+        {
+            newPartner.setPhoto(oldPhoto);
+        } else
+        {
+            newPartner.setPhoto(photoService.save(file));
+        }
+
+        newPartner.setId(id);
+        partnerRepository.save(newPartner);
+
+        response.setMessage("Successfully updated");
+        response.setData(newPartner);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 
 
     public ResponseEntity<ApiResponse<PartnerDTO>> getById(Long id)
